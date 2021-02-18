f32bf7f73dc4ee6ae3eae0026db0dca506cb09de
user <ti.ervany@gmail.com>
2021-02-19 00:08:45 +0700

    new

--- Ervany Septa Prawara.A-10-4C-Tugas P4/Checkbutton-TugasP4.py
+++ Ervany Septa Prawara.A-10-4C-Tugas P4/Checkbutton-TugasP4.py
@@ -1,18 +1,19 @@
-import tkinter
+# import tkinter
+from tkinter import *
+# from tkinter import IntVar
 
 
 def main():
-    mainform = tkinter.Tk()
+    mainform = Tk()
 
     mainform.wm_title("Checkbutton")
     mainform.geometry('500x200')
 
-    lbl1 = tkinter.Label(mainform)
+    lbl1 = Label(mainform)
     lbl1['text'] = "Checkbutton    :"
     lbl1.grid(row=0, column=0)
 
-    chk1 = tkinter.Checkbutton(mainform)
-    # chk1['text']="sdadsa"
+    chk1 = Checkbutton(mainform)
     chk1['activebackground']="green"
     chk1['activeforeground']="red"
     chk1['bg']="yellow"
@@ -20,12 +21,41 @@ def main():
     chk1['bd']=9
     chk1['command']=lbl1.destroy
     chk1['cursor']='plus'
-    # chk1['disabledforeground']="white"
-    # chk1['font']="calibri"
-    # chk1['fg']="yellow"
     chk1['height']=30
-    chk1['highlightcolor']="white"
+    chk1['relief']='sunken'
+    chk1['state']='active'
+    chk1['width']=40
     chk1.grid(row=0, column=1)
+
+    lbl2 = Label(mainform)
+    lbl2['text'] = "Checkbutton 2   :"
+    lbl2.grid(row=1, column=0)
+
+    def tampil() :
+        lblchk = Label(mainform)
+        lblchk['text']=chk.get()
+        lblchk.grid(row=1, column=2)
+
+    chk2 = Checkbutton(mainform)
+    chk=StringVar()
+    chk2['text']="Check Button"
+    chk2['variable']=chk
+    chk2['command'] = tampil
+    chk2['font']="calibri"
+    chk2['justify'] = "left"
+    chk2['offvalue']="Off"
+    chk2['onvalue']="On"
+    chk2['padx']=30
+    chk2['pady']=30
+    chk2['selectcolor']="yellow"
+    chk2['underline']=1
+    chk2['wraplength']=80
+    chk2.grid(row=1, column=1)
+
+    lbl3 = Label(mainform)
+    lbl3['text'] = "Checkbutton 3   :"
+    lbl3.grid(row=2, column=0)
+
     
     mainform.mainloop()
 
